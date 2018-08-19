2a20625af31eaaf2b5af894738771f5cf5c3be1c
Eddie <Eddie@Edwins-MacBook-Pro.local>
2018-08-18 17:22:52 -0700

    Created development environment

---
+++ src/components/App/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class App extends React.Component
+{
+  render()
+  {
+    return(
+      <section>
+        <h1>Testing</h1>
+      </section>
+    )
+  }
+}
+
+export default App;
