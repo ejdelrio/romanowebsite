f102d22bf3615dac010d38be927a22276299ba2e
Eddie <Eddie@Edwins-MacBook-Pro.local>
2018-08-19 13:18:00 -0700

    Changed casing on directory name

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import ReactDom from 'react-dom';
 import React from 'react';
 
-import App from './components/app';
+import App from './components/App';
 import './styles/style.scss';
-console.log(App);
+
 ReactDom.render(<App />, document.getElementById('root'));
